460898a6c9b7196873c9d8593ee3c3b6bed62bc6
Cat Norris <cannenorris@yahoo.com>
2018-03-06 05:41:45 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,8 +8,12 @@ def initialize (name)
 end
 
 def add_student(student_name, grade)
-  roster[grade] = []
-  roster[grade] << student_name
+  if @roster[grade] 
+  @roster[grade] << student_name
+  else
+  @roster[grade] = []
+  @roster[grade] << student_name
+  end
 end
 
 def grade (student_grade)
