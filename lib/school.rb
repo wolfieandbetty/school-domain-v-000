51f4af3524be87651ac2be711724181bf9e142c5
Cat Norris <cannenorris@yahoo.com>
2018-03-06 05:18:10 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,3 +1,27 @@
 # code here!
-school = School.new("Bayside High School")
+class School
+attr_acessor :name,:roster
+
+def initialize (name)
+  @name = name
+  @roster = {}
+end 
+
+def add_student(student_name, grade)
+  roster[grade] || = []
+  roster[grade] << student_name
+end 
+
+def grade (student_grade)
+  roster [student_grade]
+end
+
+def sort
+  sorted = {}
+  roster.each do |grade, students|
+    sorted[grade] = students.sort
+  end
+  sorted
+end
+
 end
